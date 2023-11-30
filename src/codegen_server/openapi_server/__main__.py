efd237cb987548e425e22d9dcebdb677ad44b938
NSaudagar <narayankar.saudagar4@gmail.com>
2023-12-01 03:49:17 +0530

    new changes

--- src/codegen_server/openapi_server/__main__.py
+++ src/codegen_server/openapi_server/__main__.py
@@ -6,8 +6,8 @@ import os
 from openapi_server.run import application
 
 def main():
-    port = os.getenv("PORT") or 5000
-    application.run(port=port)
+    port = os.getenv("PORT") or 8080
+    application.run(port=port, debug=True)
 
 if __name__ == '__main__':
     main()
